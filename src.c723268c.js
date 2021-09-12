ddcdfcd0b0544d9693a2e701ed4523bea9d1430d
stargazinghero <87721107+stargazinghero@users.noreply.github.com>
2021-09-12 11:54:02 +0000

    Deploying to gh-pages from @ stargazinghero/test-parcel@a13b761891f517a8026379512a38d32825851d42 🚀

--- src.c723268c.js
+++ src.c723268c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/img-hero.jpg":[["img-hero.42024e22.jpg","awAC"],"awAC"],"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test-parcel/src.a0469496.js.map
+//# sourceMappingURL=/test-parcel/src.c723268c.js.map
